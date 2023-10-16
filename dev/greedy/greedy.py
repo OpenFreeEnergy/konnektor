c089e5f42cdb8b2563583a7c6c8532784d552bf8
bries <benjamin-ries@outlook.com>
2023-10-16 11:29:11 +0200

    add dev scripts

--- dev/greedy/greedy.py
+++ dev/greedy/greedy.py
@@ -1,12 +1,13 @@
-import numpy as np
 import datetime as dt
+
+import numpy as np
 from konnektor.network_generator_algorithms import CyclicNetworkGenerator
 
 sub_cycle_size_range = 3
 node_cycle_connectivity = 2 #a node should be at least in n cycles
 
-#Test Data: $10, 20, 30, 40, 50, 60,70,80, 90, 100, 300, 500, 700, 1000, 5000, 6000, 
-steps = [10, 20, 30, 40, 50, 60,70,80, 90, 100, 300, 500, 700, 1000, 5000, 6000] #
+#Test Data: $10, 20, 30, 40, 50, 60,70,80, 90, 100, 300, 500, 700, 1000, 5000, 6000,
+steps = [10, 20, 30, 40, 50, 60,70,80, 90, 100, 300] #
 cgs=[]
 for n in steps: #range(10, 81, 10):
     print("Number of nodes:", n)
@@ -16,9 +17,8 @@ for n in steps: #range(10, 81, 10):
 
     network_planner = CyclicNetworkGenerator(node_cycle_connectivity=node_cycle_connectivity,
                                              sub_cycle_size_range=sub_cycle_size_range)
-
     start = dt.datetime.now()
-    cg = network_planner.generate_network_double_greedy(edges=edges, weights=weights)
+    cg = network_planner.generate_network(edges=edges, weights=weights)
     cgs.append([network_planner.orig_g, cg])
     end = dt.datetime.now()
     duration = end-start
