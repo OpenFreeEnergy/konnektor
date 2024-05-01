fb9425f06208841a84c45d788058629f72e83946
riesben <benjamin-ries@outlook.com>
2024-05-01 20:24:43 +0200

    refactor data

--- src/konnektor/data/__init__.py
+++ src/konnektor/data/__init__.py
@@ -1,4 +1,20 @@
 import os
 
+from rdkit import Chem
+
+from gufe import SmallMoleculeComponent
+
 root_path = os.path.dirname(__file__)
 benzenes_sdf_path = f"{root_path}/benzenes_RHFE.sdf"
+
+def get_benzene_compounds()->list[SmallMoleculeComponent]:
+    """
+    get the benzene test dataset parsed as SmallMoleculeComponents.
+
+    Returns
+    -------
+    list[SmallMoleculeComponent]
+        a list of the benzene compounds.
+
+    """
+    return [SmallMoleculeComponent.from_rdkit(rdm) for rdm in Chem.SDMolSupplier(benzenes_sdf_path, removeHs=False)]
