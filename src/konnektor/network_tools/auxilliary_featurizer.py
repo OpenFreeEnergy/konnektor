46c28f00b540afe7c8d7e8b8e3dcb729ce54a6bd
riesben <benjamin-ries@outlook.com>
2024-05-05 22:56:08 +0200

    Refactor: - tools refactoring

---
+++ src/konnektor/network_tools/auxilliary_featurizer.py
@@ -0,0 +1,36 @@
+from typing import Union
+
+import numpy as np
+from rdkit import Chem
+from scikit_mol.fingerprints import FpsTransformer
+from sklearn.base import ClusterMixin
+
+
+class ChargeTransformer(FpsTransformer):
+    def __init__(self, parallel: Union[bool, int] = False
+                 ):
+        """Calculates the RDKit FormalCharge and provides it as single field vector.
+
+        Parameters
+        ----------
+        parallel: Union[bool, int], optional
+        Whether to parallelize the calculations, default: False
+
+        """
+        super().__init__(parallel=parallel)
+        self.nBits = 1
+
+    @property
+    def fpSize(self):
+        return self.nBits
+
+    # Scikit-Learn expects to be able to set fpSize directly on object via .set_params(), so this updates nBits used by the abstract class
+    @fpSize.setter
+    def fpSize(self, fpSize):
+        self.nBits = fpSize
+
+    def _mol2fp(self, mol):
+        pass
+
+    def _transform_mol(self, mol):
+        return np.array([Chem.GetFormalCharge(mol)])
