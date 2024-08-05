7e6250edff5651fc05a41fb1eea02e1ff6db4d7e
RiesBen <benjamin-ries@outlook.com>
2024-08-05 10:20:56 +0200

    example: adding charge data

--- src/konnektor/data/__init__.py
+++ src/konnektor/data/__init__.py
@@ -10,6 +10,7 @@ from gufe import SmallMoleculeComponent
 root_path = os.path.dirname(__file__)
 benzenes_sdf_path = f"{root_path}/benzenes_RHFE.sdf"
 hif2a_sdf_path = f"{root_path}/hif2a_ligands.sdf"
+charged_ligands_path = f"{root_path}/charged_ligands.sdf"
 
 def get_benzene_ligands()->list[SmallMoleculeComponent]:
     """
@@ -34,3 +35,15 @@ def get_hif2a_ligands()->list[SmallMoleculeComponent]:
 
     """
     return [SmallMoleculeComponent.from_rdkit(rdm) for rdm in Chem.SDMolSupplier(hif2a_sdf_path, removeHs=False)]
+   
+def get_charged_ligands()->list[SmallMoleculeComponent]:
+    """
+    get the benzene test dataset parsed as SmallMoleculeComponents.
+
+    Returns
+    -------
+    list[SmallMoleculeComponent]
+        a list of the benzene compounds.
+
+    """
+    return [SmallMoleculeComponent.from_rdkit(rdm) for rdm in Chem.SDMolSupplier(charged_ligands_path, removeHs=False)]
