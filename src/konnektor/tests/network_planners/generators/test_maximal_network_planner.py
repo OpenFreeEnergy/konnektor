9136da0cdcd302f91d8f78744fa2a8df60fb5e58
Alyssa Travitz <atravitz@umich.edu>
2024-11-15 13:22:55 -0800

    adding ugly but working test

--- src/konnektor/tests/network_planners/generators/test_maximal_network_planner.py
+++ src/konnektor/tests/network_planners/generators/test_maximal_network_planner.py
@@ -10,6 +10,33 @@ from konnektor.tests.network_planners.conf import (
     genScorer,
     GenAtomMapper,
 )
+from gufe import LigandAtomMapping, AtomMapper, AtomMapping
+from konnektor.utils.toy_data import build_random_dataset
+
+
+class CounterTestMapper(AtomMapper):
+    def __init__(self):
+        """
+        Build a generic Mapper, that only has use for dummy mappings.
+        Generates mappings that increment by 1 for testing order-dependent behavior.
+        """
+        pass
+
+    def suggest_mappings(self, molA, molB) -> AtomMapping:
+        yield LigandAtomMapping(molA, molB, {0:1})
+
+    @classmethod
+    def _defaults(cls):
+        return super()._defaults()
+
+    @classmethod
+    def _from_dict(cls, d):
+        s = cls()
+        [setattr(s, k, v) for k, v in d.items()]
+        return s
+
+    def _to_dict(self):
+        return vars(self)
 
 
 @pytest.mark.parametrize("n_process", [1, 2])
@@ -41,3 +68,18 @@ def test_generate_maximal_network(
     else:
         for edge in network.edges:
             assert "score" not in edge.annotations
+
+def test_generate_maximal_network_missing_scorer():
+    """If no scorer is provided, the last mapper tried should be used."""
+
+    components, empty_mapper, _ = build_random_dataset(n_compounds=2)
+    counter_mapper = CounterTestMapper()
+    planner = MaximalNetworkGenerator(
+        mappers= [empty_mapper, counter_mapper],
+        scorer=None,
+        progress=False,
+        n_processes=1,
+    )
+    network = planner.generate_ligand_network(components)
+    assert [e for e in network.edges][-1].componentA_to_componentB == {0:1}
+    [(e.componentA.name, e.componentB.name) for e in network.edges]
