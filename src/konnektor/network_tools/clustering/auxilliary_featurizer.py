6f024acf0106c3cd36a6918f58e6f1764209ca8d
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-02-24 12:13:48 -0800

    update parallel to n_jobs

--- src/konnektor/network_tools/clustering/auxilliary_featurizer.py
+++ src/konnektor/network_tools/clustering/auxilliary_featurizer.py
@@ -1,6 +1,7 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/konnektor
 
+from typing import Optional
 
 import numpy as np
 from rdkit import Chem
@@ -8,16 +9,18 @@ from scikit_mol.fingerprints import FpsTransformer
 
 
 class ChargeTransformer(FpsTransformer):
-    def __init__(self, parallel: bool | int = False):
+    def __init__(self, n_jobs: Optional[int] = None):
         """Calculates the RDKit FormalCharge and provides it as single field vector.
 
         Parameters
         ----------
-        parallel: Union[bool, int], optional
+        n_jobs : int, optional default=None
+            The maximum number of concurrently running jobs.
+            None is a marker for 'unset' that will be interpreted as n_jobs=1 unless the call is performed under a parallel_config()
         Whether to parallelize the calculations, default: False
 
         """
-        super().__init__(parallel=parallel)
+        super().__init__(n_jobs=n_jobs)
         self.fpSize = 1
 
     def _mol2fp(self, mol):
