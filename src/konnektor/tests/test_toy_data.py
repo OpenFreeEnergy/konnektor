3ce47acb8349b3edd78f4a19756876bb3f651d0b
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-02-25 11:26:16 -0800

    fixing test

--- src/konnektor/tests/test_toy_data.py
+++ src/konnektor/tests/test_toy_data.py
@@ -122,10 +122,10 @@ def test_build_n_random_mst_network(n_sub_networks, overlap):
 
 
 # rename this so it gets picked up by pytest
-def _build_random_fully_connected_network():
+def test_build_random_fully_connected_network():
     n_compounds = 30
     mst_network = build_random_fully_connected_network(n_compounds=n_compounds, rand_seed=42)
 
     assert isinstance(mst_network, LigandNetwork)
     assert len(mst_network.nodes) == n_compounds
-    assert len(mst_network.edges) == n_compounds * (n_compounds - 1)
+    assert len(mst_network.edges) == (n_compounds * (n_compounds - 1))/2
