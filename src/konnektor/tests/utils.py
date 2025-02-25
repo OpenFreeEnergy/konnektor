ab6588a78a78a0c4eb6f499d035389d6f43733de
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-02-24 16:36:28 -0800

    adding todo to mystery test utils

--- src/konnektor/tests/utils.py
+++ src/konnektor/tests/utils.py
@@ -1,6 +1,6 @@
 # This code is part of OpenFE and is licensed under the MIT license.
 # For details, see https://github.com/OpenFreeEnergy/konnektor
 
-
+# TODO: remove this?
 def test_network_merging(nine_mols_edges_two_sets):
     network1, network2 = nine_mols_edges_two_sets
