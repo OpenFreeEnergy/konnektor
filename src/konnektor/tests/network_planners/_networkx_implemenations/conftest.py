8192d4861842891c0233d548050acb64bd604e4b
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-03-11 15:49:17 -0700

    removing unnecessary session scope

--- src/konnektor/tests/network_planners/_networkx_implemenations/conftest.py
+++ src/konnektor/tests/network_planners/_networkx_implemenations/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def nine_mols_edges():
     data = [
         ["lig_12", "lig_14", 0.29531191028128656],
@@ -45,7 +45,7 @@ def nine_mols_edges():
     return data
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def nine_mols_edges_two_sets():
     data = [
         ["lig_12", "lig_14", 0.29531191028128656],
