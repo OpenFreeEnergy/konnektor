b951ace04a5e62446ee0f31ab08f0d37d5f1ff36
Alyssa Travitz <31974495+atravitz@users.noreply.github.com>
2025-04-09 10:03:33 -0700

    Fix charge transformer api (#133)

--- src/konnektor/network_tools/clustering/auxilliary_featurizer.py
+++ src/konnektor/network_tools/clustering/auxilliary_featurizer.py
@@ -4,24 +4,23 @@
 
 import numpy as np
 from rdkit import Chem
-from scikit_mol.fingerprints import FpsTransformer
+from scikit_mol.fingerprints.baseclasses import BaseFpsTransformer
 
 
-class ChargeTransformer(FpsTransformer):
-    def __init__(self, parallel: bool | int = False):
+class ChargeTransformer(BaseFpsTransformer):
+    def __init__(self, n_jobs: int | None = None):
         """Calculates the RDKit FormalCharge and provides it as single field vector.
 
         Parameters
         ----------
-        parallel: Union[bool, int], optional
+        n_jobs : int, optional default=None
+            The maximum number of concurrently running jobs.
+            None is a marker for 'unset' that will be interpreted as n_jobs=1 unless the call is performed under a parallel_config()
         Whether to parallelize the calculations, default: False
 
         """
-        super().__init__(parallel=parallel)
+        super().__init__(n_jobs=n_jobs)
         self.fpSize = 1
-
-    def _mol2fp(self, mol):
-        pass
 
     def _transform_mol(self, mol):
         return np.array([Chem.GetFormalCharge(mol)])
