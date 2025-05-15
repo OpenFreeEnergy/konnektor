6a0d5e55e8d2f75acc5e7b15bc07fedeb33178a7
Alyssa Travitz <31974495+atravitz@users.noreply.github.com>
2025-05-15 13:35:04 -0700

    [WIP] Star generator refactor (#131)
    * split serial mapping into its own function
    
    * switch heuristic max generator to use shared function
    
    * moving into shared _map_scoring.py
    
    * updating star map generator
    
    * removing duplicated code
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/konnektor/network_planners/generators/star_network_generator.py
+++ src/konnektor/network_planners/generators/star_network_generator.py
@@ -2,7 +2,6 @@
 # For details, see https://github.com/OpenFreeEnergy/konnektor
 
 import functools
-import warnings
 from collections.abc import Iterable
 
 from gufe import AtomMapper, Component, LigandNetwork
@@ -10,6 +9,7 @@ from tqdm import tqdm
 
 from konnektor.network_planners._networkx_implementations import RadialNetworkAlgorithm
 
+from .._map_scoring import _determine_best_mapping
 from ._abstract_network_generator import NetworkGenerator
 from .maximal_network_generator import MaximalNetworkGenerator
 
@@ -108,12 +108,6 @@ class StarNetworkGenerator(NetworkGenerator):
             selected_mappings = [edge_map[k] for k in rg.edges]
 
         else:  # Given central ligands: less effort. - Trivial Case
-            # TODO: commenting this out because scorer isn't used - should it be used?
-            # if self.scorer is None:
-            #     scorer = lambda x: -1
-            # else:
-            #     scorer = self.scorer
-
             if self.progress is True:
                 progress = functools.partial(tqdm, total=len(components), delay=1.5, desc="Mapping")
             else:
@@ -121,43 +115,18 @@ class StarNetworkGenerator(NetworkGenerator):
 
             selected_mappings = []
             for component in progress(components):
-                best_score = 0.0
-                best_mapping = None
-                molA = central_component
-                molB = component
-
-                for mapper in self.mappers:
-                    mapping_generator = mapper.suggest_mappings(molA, molB)
-
-                    if self.scorer:
-                        tmp_mappings = [
-                            mapping.with_annotations({"score": self.scorer(mapping)})
-                            for mapping in mapping_generator
-                        ]
-
-                        if len(tmp_mappings) > 0:
-                            tmp_best_mapping = min(
-                                tmp_mappings, key=lambda m: m.annotations["score"]
-                            )
-
-                            if (
-                                tmp_best_mapping.annotations["score"] < best_score
-                                or best_mapping is None
-                            ):
-                                best_score = tmp_best_mapping.annotations["score"]
-                                best_mapping = tmp_best_mapping
-                    else:
-                        try:
-                            # TODO: this is duplicated code, use _map_scoring
-                            warnings.warn(
-                                f"Multiple mappers were provided, but no scorer. Only the first mapper provided will be used: {mapper}"
-                            )
-                            best_mapping = next(mapping_generator)
-                            break
-                        except:
-                            continue
+                best_mapping = _determine_best_mapping(
+                    component_pair=(central_component, component),
+                    mappers=self.mappers,
+                    scorer=self.scorer,
+                )
                 if best_mapping is not None:
                     selected_mappings.append(best_mapping)
+
+        if len(selected_mappings) == 0:
+            raise RuntimeError("Could not generate any mapping!")
+
+        # TODO: raise a warning if some nodes are disconnected?
 
         return LigandNetwork(edges=selected_mappings, nodes=components)
 
