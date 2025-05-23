642472fbfbf5103f952361fe810b528aa3c9abd7
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-05-23 09:39:35 -0700

    docstrings and TODOs

--- src/konnektor/network_planners/generators/maximal_network_generator.py
+++ src/konnektor/network_planners/generators/maximal_network_generator.py
@@ -105,5 +105,6 @@ class MaximalNetworkGenerator(NetworkGenerator):
         if len(mappings) == 0:
             raise RuntimeError("Could not generate any mapping!")
 
+        # TODO: raise an error? warning? if resulting network is disconnected
         network = LigandNetwork(edges=mappings, nodes=components)
         return network
