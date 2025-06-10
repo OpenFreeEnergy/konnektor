b9ffa7daded221af6997ac8883a1794d48b52287
Alyssa Travitz <31974495+atravitz@users.noreply.github.com>
2025-06-10 13:41:14 -0700

    refactor to make edge ordering match openfe (#159)

--- src/konnektor/network_planners/_networkx_implementations/mst_network_algorithm.py
+++ src/konnektor/network_planners/_networkx_implementations/mst_network_algorithm.py
@@ -14,7 +14,7 @@ class MstNetworkAlgorithm(_AbstractNetworkAlgorithm):
         wedges = []
         nodes = []
         # The initial "weights" are Scores, which need to be translated to weights.
-        weights = list(map(lambda x: 1 - x, weights))
+        weights = list(map(lambda x: -x, weights))
         for edge, weight in zip(edges, weights):
             wedges.append([edge[0], edge[1], weight])
             nodes.extend(list(edge))
