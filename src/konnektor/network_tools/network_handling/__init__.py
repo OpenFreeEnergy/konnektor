2437409b7127983fd2226918465c7036a01a06d2
Alyssa Travitz <alyssa.travitz@omsf.io>
2025-08-26 10:42:12 -0700

    fix __all__ lists

--- src/konnektor/network_tools/network_handling/__init__.py
+++ src/konnektor/network_tools/network_handling/__init__.py
@@ -6,10 +6,10 @@ from .delete import delete_component, delete_transformation
 from .merge import merge_networks, merge_two_networks
 
 __all__ = [
-    append_component,
-    concatenate_networks,
-    delete_component,
-    delete_transformation,
-    merge_networks,
-    merge_two_networks,
+    "append_component",
+    "concatenate_networks",
+    "delete_component",
+    "delete_transformation",
+    "merge_networks",
+    "merge_two_networks",
 ]
