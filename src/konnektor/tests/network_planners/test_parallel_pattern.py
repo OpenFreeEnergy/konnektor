6be5b906135ef0d6521883d2075f0d4261065582
Alyssa Travitz <alyssa.travitz@omsf.io>
2026-01-27 11:53:00 -0800

    add failing test

--- src/konnektor/tests/network_planners/test_parallel_pattern.py
+++ src/konnektor/tests/network_planners/test_parallel_pattern.py
@@ -5,6 +5,7 @@ from gufe import ComponentMapping
 
 from konnektor.network_planners._map_scoring import (
     _parallel_map_scoring,
+    _serial_map_scoring,
     thread_mapping,
 )
 
@@ -37,3 +38,30 @@ def test_parallel_map_scoring(with_progress, n_process):
 
     assert len(mappings) == len(component_pairs)
     assert all(isinstance(m, ComponentMapping) for m in mappings)
+
+
+def test_parallel_serial_equality():
+    components, mapper, scorer = build_random_dataset(n_compounds=20)
+    component_pairs = list(itertools.combinations(components, 2))
+
+    mappings_parallel = _parallel_map_scoring(
+        possible_edges=component_pairs,
+        scorer=scorer,
+        mappers=[mapper],
+        n_processes=1,
+        show_progress=False,
+    )
+    mappings_serial = _serial_map_scoring(
+        possible_edges=component_pairs,
+        scorer=scorer,
+        mappers=[mapper],
+        n_edges_to_score=len(component_pairs),
+        show_progress=False,
+    )
+    assert len(mappings_parallel) == len(mappings_serial)
+    comps_serial = {(m.componentA, m.componentB) for m in mappings_serial}
+    comps_parallel = {(m.componentA, m.componentB) for m in mappings_parallel}
+    assert comps_serial == comps_parallel
+    assert set(mappings_parallel) == set(mappings_serial)
+    assert len(mappings_parallel) == len(component_pairs)
+    assert all(isinstance(m, ComponentMapping) for m in mappings_parallel)
