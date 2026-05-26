7cf40a0101537ba5a8b81829e91c6fe750c680e6
Alyssa Travitz <alyssa.travitz@omsf.io>
2026-05-26 13:51:16 -0700

    update docstrings

--- src/konnektor/network_planners/generators/_abstract_network_generator.py
+++ src/konnektor/network_planners/generators/_abstract_network_generator.py
@@ -18,10 +18,11 @@ class NetworkGenerator(NetworkPlanner):
         self,
         mappers: AtomMapper | Iterable[AtomMapper] | None,
         scorer: Callable[[AtomMapping], float] | None,
-        network_generator: _AbstractNetworkAlgorithm,  # TODO: rename this network_algorithm and make base class not private
+        # TODO: rename this to network_algorithm and make base class not private?
+        network_generator: _AbstractNetworkAlgorithm | None,
         n_processes: int = 1,
         progress: bool = False,
-        _initial_edge_lister=None,  # TODO: why is this private, deprecate and rename?
+        _initial_edge_lister=None,  # TODO: why is this private, can we deprecate and rename?
     ):
         """Abstract class for network generation, not to be called directly.
 
@@ -31,7 +32,7 @@ class NetworkGenerator(NetworkPlanner):
             AtomMapper(s) to use to propose mappings.
         scorer : Callable[[AtomMapping], float] | None
             Callable which takes a AtomMapping and returns a float in [0,1].
-        network_generator : _AbstractNetworkAlgorithm
+        network_generator : _AbstractNetworkAlgorithm | None.
             Algorithm to use when generating the network.
         n_processes : int, optional
             Number of processes that can be used for the network generation, by default 1.
@@ -39,7 +40,7 @@ class NetworkGenerator(NetworkPlanner):
             If True, display a progress bar, by default False
         _initial_edge_lister : _type_, optional
             The NetworkPlanner to use to create the initial set of edges. For standard usage, the MaximalNetworkPlanner is used.
-            However in large scale approaches, it might be interesting to use the heuristicMaximalNetworkPlanner, by default None
+            However in large scale approaches, it might be interesting to use the heuristicMaximalNetworkPlanner, by default None.
         """
 
         super().__init__(mappers=mappers, scorer=scorer)
@@ -61,9 +62,7 @@ class NetworkGenerator(NetworkPlanner):
 
     @property
     def progress(self) -> bool:
-        """
-        shows a progress bar if True
-        """
+        """If True, displays a progress bar when generating the LigandNetwork."""
         return self._progress
 
     @progress.setter
@@ -74,16 +73,16 @@ class NetworkGenerator(NetworkPlanner):
 
     @abc.abstractmethod
     def generate_ligand_network(self, components: Iterable[Component]) -> LigandNetwork:
-        """Plan a Network which attempts to connect ligands following a given algorithm.
+        """Generate a LigandNetwork with the given Components as nodes and using this NetworkPlanner's mappers and scorer to create AtomMapping edges.
 
         Parameters
         ----------
         components : Iterable[Component]
-            The Components to include as nodes in the ligand network.
+            Component(s) to include in the Network
 
         Returns
         -------
         LigandNetwork
-            The resulting ligand network.
+            LigandNetwork with Components as nodes and generated AtomMappings as edges.
         """
         raise NotImplementedError()
