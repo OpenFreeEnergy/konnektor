d27571aa86e68e034cb772d0f09b16e9130d2a7c
Alyssa Travitz <alyssa.travitz@omsf.io>
2026-05-28 12:23:25 -0700

    update __all__ imports to be strings

--- src/konnektor/network_tools/network_handling/__init__.py
+++ src/konnektor/network_tools/network_handling/__init__.py
@@ -6,9 +6,9 @@ from .delete import delete_component, delete_transformation
 from .merge import merge_networks, merge_two_networks
 
 __all__ = [
-    append_component,
-    delete_component,
-    delete_transformation,
-    merge_networks,
-    merge_two_networks,
+    "append_component",
+    "delete_component",
+    "delete_transformation",
+    "merge_networks",
+    "merge_two_networks",
 ]
