b03b4b23f5104b79d10de6a0b1b8c6a5a2e1cb97
hannahbaumann <hbaumann@uci.edu>
2026-09-07 11:45:36 +0200

    update tests

--- src/konnektor/tests/network_planners/generators/test_clustered_network_generator.py
+++ src/konnektor/tests/network_planners/generators/test_clustered_network_generator.py
@@ -40,10 +40,9 @@ def test_clustered_network_planner():
     assert len(ligand_network.nodes) == n_compounds
     n_clusters = len(planner.clusters)
     assert n_clusters == 3
-    expected_number_of_edges = (n_compounds - n_clusters) + (
-        n_clusters - 1
-    ) * concatenator.n_connecting_edges
+    expected_number_of_edges = (n_compounds - n_clusters) + n_clusters - 1
+
     assert len(ligand_network.edges) == expected_number_of_edges
     assert ligand_network.is_connected()
 
-    np.testing.assert_allclose(get_network_score(ligand_network), 25.708691, rtol=0.05)
+    np.testing.assert_allclose(get_network_score(ligand_network), 24.166487, rtol=0.05)
