25d42d3251e3095f764c7e36e2e2ca4d7bbdba99
hannahbaumann <hbaumann@uci.edu>
2026-09-07 10:05:41 +0200

    missing import

--- src/konnektor/tests/network_tools/test_connected_subnetworks.py
+++ src/konnektor/tests/network_tools/test_connected_subnetworks.py
@@ -4,7 +4,7 @@
 from gufe import LigandNetwork
 
 from konnektor.network_tools.network_handling.connected_subnetworks import connected_subnetworks
-from konnektor.utils.toy_data import build_n_random_mst_network, build_random_mst_network
+from konnektor.utils.toy_data import build_n_random_mst_network, build_random_mst_network, build_random_dataset
 
 
 def test_connected_network_single_subnetwork():
